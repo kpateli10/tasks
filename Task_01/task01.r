38ffd67e0c9aaa727af74e41dc9779ba2daefcf4
kpateli10 <kpateli10@icloud.com>
2020-01-17 00:19:25 +0200

    Konstantina Pateli Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,2 +1,23 @@
-install.packages(''swirl'')
-library (swirl)
+install.packages("swirl")
+library(swirl)
+Konstantina Pateli
+5+7
+x<- 5 + 7
+x
+y<- x - 3
+y
+c(1.1, 9, 3.14)
+z<- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 100
+my_sqrt<- sqrt(z - 1)
+2
+my_div<- z/ my_sqrt
+3
+my_div
+c(1, 2, 3, 4) + c(0, 100)
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+bye
