6d9c735ddc43ff154570cb8ea84aed26014ac08e
kpateli10 <kpateli10@icloud.com>
2020-01-22 19:02:27 -0500

    Konstantina Pateli Task 02 upload b

--- Task.02/Task02.R
+++ Task.02/Task02.R
@@ -21,3 +21,34 @@ head(Data)
 beren2 <- Data
 beren3 <- beren2[order(beren2$age),]
 write.csv(beren3, "beren_new.csv", quote=F, row.names=FALSE)
+
+2b
+Feeds <- which(beren3$event == "bottle")
+avgMilk <- mean(beren3$value[Feeds])
+avgFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], mean)
+varFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], var)
+totalFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], sum)
+numFeeds <- tapply(beren3$value[Feeds], beren3$age[Feeds], length)
+cor(beren3$value[Feeds], beren3$age[Feeds])
+cor.test(beren3$value[Feeds], beren3$age[Feeds])
+berenCor <- cor.test(beren3$value[Feeds], beren3$age[Feeds])
+summary(berenCor)
+berenANOVA <- aov(beren3$value[Feeds] ~ beren3$caregiver[Feeds])
+boxplot( beren3$value[Feeds] ~ beren3$caregiver[Feeds], xlab= "who gave the bottle", ylab= "amount of milk consumed (oz)" )
+par(las=1, mar=c(5,5,1,1), mgp=c(2, 0.5, 0), tck= -0.01)
+plot(as.numeric(names(totalFeed)), totalFeed, type="b", pch=16, xlab="age in days", ylab="ounces of milk")
+abline(h=mean(totalFeed),lty=2, col='red')
+pdf("r02b-totalMilkByDay.pdf", height = 4, width = 4)
+par(las=1, mar=c(5,5,1,1), mgp=c(2, 0.5, 0), tck=-0.01)
+plot(as.numeric(names( totalFeed)), totalFeed, type="b", pch=16, xlba="age in days", ylab="ounces of milk")
+abline(h=mean(totalFeed), lty=2, col="red")
+dev.off()
+source("http://jonsmitchell.com/code/plotFxn02b.R") 
+unique(beren3$event)
+
+
+
+
+
+
+
